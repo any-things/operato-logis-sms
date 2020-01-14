56b40f4d0cfb4c4599cd6fad50ad20e94aac6601
hatiolab <shortstop@hatiolab.com>
2020-01-14 12:46:41 +0900

    initial import

---
+++ src/main/java/operato/logis/sms/SmsConstants.java
@@ -0,0 +1,12 @@
+package operato.logis.sms;
+
+import xyz.anythings.base.LogisConstants;
+
+/**
+ * SMS 모듈 상수 정의 
+ * 
+ * @author shortstop
+ */
+public class SmsConstants extends LogisConstants {		
+
+}
---
+++ src/main/java/operato/logis/sms/query/SmsQueryStore.java
@@ -0,0 +1,23 @@
+package operato.logis.sms.query;
+
+import org.springframework.stereotype.Component;
+
+import xyz.anythings.sys.service.AbstractQueryStore;
+import xyz.elidom.sys.SysConstants;
+
+/**
+ * 반품 프로세스
+ * 
+ * @author shortstop
+ */
+@Component
+public class SmsQueryStore extends AbstractQueryStore {
+
+	@Override
+	public void initQueryStore(String databaseType) {
+		this.databaseType = databaseType;
+		this.basePath = "operato/logis/sms/query/" + this.databaseType + SysConstants.SLASH;
+		this.defaultBasePath = "operato/logis/sms/query/ansi/"; 
+	}
+	
+}
