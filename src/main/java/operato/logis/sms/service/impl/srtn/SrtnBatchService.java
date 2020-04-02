1ab2f85bd19b539e57eec94007e3044ff40e92e4
hatiolab <shortstop@hatiolab.com>
2020-04-02 17:58:20 +0900

    배치 마감 시 이벤트 처리 로직 추가

--- src/main/java/operato/logis/sms/service/impl/srtn/SrtnBatchService.java
+++ src/main/java/operato/logis/sms/service/impl/srtn/SrtnBatchService.java
@@ -11,13 +11,14 @@ import xyz.anythings.base.LogisConstants;
 import xyz.anythings.base.entity.JobBatch;
 import xyz.anythings.base.entity.JobInstance;
 import xyz.anythings.base.entity.Order;
+import xyz.anythings.base.event.main.BatchCloseEvent;
 import xyz.anythings.base.model.BatchProgressRate;
 import xyz.anythings.base.service.api.IBatchService;
 import xyz.anythings.base.service.impl.AbstractLogisService;
+import xyz.anythings.sys.event.model.SysEvent;
 import xyz.anythings.sys.util.AnyOrmUtil;
 import xyz.elidom.dbist.dml.Filter;
 import xyz.elidom.dbist.dml.Query;
-import xyz.elidom.exception.server.ElidomValidationException;
 import xyz.elidom.orm.OrmConstants;
 import xyz.elidom.sys.util.MessageUtil;
 import xyz.elidom.sys.util.ThrowUtil;
@@ -39,7 +40,16 @@ public class SrtnBatchService extends AbstractLogisService implements IBatchServ
 		
 	@Override
 	public void isPossibleCloseBatch(JobBatch batch, boolean closeForcibly) {
-		// 1. 작업 배치 상태 체크
+		// 1. 배치 마감 전 처리 이벤트 전송
+		BatchCloseEvent event = new BatchCloseEvent(batch, SysEvent.EVENT_STEP_BEFORE);
+		event = (BatchCloseEvent)this.eventPublisher.publishEvent(event);
+		
+		// 2. 이벤트 취소라면 ...
+		if(event.isAfterEventCancel()) {
+			return;
+		}
+		
+		// 3. 작업 배치 상태 체크
 		if(ValueUtil.isNotEqual(batch.getStatus(), JobBatch.STATUS_RUNNING)) {
 			// 진행 중인 작업배치가 아닙니다
 			throw ThrowUtil.newStatusIsNotIng("terms.label.job_batch");
@@ -48,27 +58,27 @@ public class SrtnBatchService extends AbstractLogisService implements IBatchServ
 		Query condition = AnyOrmUtil.newConditionForExecution(batch.getDomainId());
 		condition.addFilter(new Filter("batchId", batch.getId()));
 
-		// 2. batchId별 수신 주문이 존재하는지 체크
+		// 4. batchId별 수신 주문이 존재하는지 체크
 		int count = this.queryManager.selectSize(Order.class, condition);
 		if(count == 0) {
 			// 해당 배치의 주문정보가 없습니다 --> 주문을 찾을 수 없습니다.
 			throw ThrowUtil.newNotFoundRecord("terms.label.order");
 		}
 
-		// 3. batchId별 작업 실행 데이터 체크
+		// 5. batchId별 작업 실행 데이터 체크
 		count = this.queryManager.selectSize(JobInstance.class, condition);
 		if(count == 0) {
 			// 해당 배치의 작업실행 정보가 없습니다 --> 작업을 찾을 수 없습니다.
 			throw ThrowUtil.newNotFoundRecord("terms.label.job");
 		}
 
-		// 4. batchId별 작업 실행 데이터 중에 완료되지 않은 것이 있는지 체크
+		// 6. batchId별 작업 실행 데이터 중에 완료되지 않은 것이 있는지 체크
 		if(!closeForcibly) {
 			condition.addFilter("status", OrmConstants.IN, LogisConstants.JOB_STATUS_WIPC);		 
 			if(this.queryManager.selectSize(JobInstance.class, condition) > 0) {
 				// {0} 등 {1}개의 호기에서 작업이 끝나지 않았습니다.
-				String msg = MessageUtil.getMessage("MPS_NOT_CLOSED_IN_REGIONS", "{0} 등 {1}개의 호기에서 작업이 끝나지 않았습니다.", ValueUtil.toList(batch.getEquipCd(), "1"));
-				throw new ElidomValidationException(msg);
+				String msg = MessageUtil.getMessage("ASSORTING_NOT_FINISHED_IN_RACKS", "{0} 등 {1}개의 호기에서 작업이 끝나지 않았습니다.", ValueUtil.toList(batch.getEquipCd(), "1"));
+				throw ThrowUtil.newValidationErrorWithNoLog(msg);
 			}
 		}
 	}
@@ -78,16 +88,25 @@ public class SrtnBatchService extends AbstractLogisService implements IBatchServ
 		// 1. 작업 마감 가능 여부 체크 
 		this.isPossibleCloseBatch(batch, forcibly);
 
-		// 2. 해당 배치에 랙, 작업 셀 정보 리셋
+		// 2. 배치 마감 후 처리 이벤트 전송
+		BatchCloseEvent event = new BatchCloseEvent(batch, SysEvent.EVENT_STEP_AFTER);
+		event = (BatchCloseEvent)this.eventPublisher.publishEvent(event);
+		
+		// 3. 이벤트 취소라면 ...
+		if(event.isAfterEventCancel()) {
+			return;
+		}
+		
+		// 4. 해당 배치에 랙, 작업 셀 정보 리셋
 		this.resetRacksAndCells(batch);
 
-		// 3. OREDER_PREPROCESS 삭제
+		// 5. OREDER_PREPROCESS 삭제
 		this.deletePreprocess(batch);
 
-		// 4. JobBatch 상태 변경
+		// 6. JobBatch 상태 변경
 		this.updateJobBatchFinished(batch, new Date());
 		
-		// 5. 분류 서비스 배치 마감 API 호출 
+		// 7. 분류 서비스 배치 마감 API 호출 
 		this.serviceDispatcher.getAssortService(batch).batchCloseAction(batch);
 	}
 
